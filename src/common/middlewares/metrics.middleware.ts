9641eb7383447ef1f5e621c3ddd77be6ff6e4132
bekhruzbekswe <bekhruzbekswe@gmail.com>
2025-09-02 11:23:01 +0500

    refactor: normalize endpoint urls in metric counting

--- src/common/middlewares/metrics.middleware.ts
+++ src/common/middlewares/metrics.middleware.ts
@@ -5,12 +5,17 @@ import { MonitoringService } from 'src/modules/monitoring/monitoring.service';
 export class MetricsMiddleware implements NestMiddleware {
   constructor(private readonly monitoringService: MonitoringService) {}
 
+  private normalizeEndpoint(path: string): string {
+    return path.replace(/\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/gi, '/:id');
+  }
+
   use(req: Request, _res: Response, next: () => void) {
     const path = req.url.split('?')[0];
+    const normalizedPath = this.normalizeEndpoint(path);
 
     this.monitoringService.incrementApiUsage({
       method: req.method,
-      endpoint: path,
+      endpoint: normalizedPath,
     });
 
     if (path.startsWith('/api/feedback/upload')) {
